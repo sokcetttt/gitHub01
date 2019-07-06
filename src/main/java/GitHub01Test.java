e2b5d0cecac2e1ca6dcf3c3d48cf181b98e93bb2
sokcetttt <52492769+sokcetttt@users.noreply.github.com>
2019-07-07 01:10:51 +0800

    Update GitHub01Test.java

--- src/main/java/GitHub01Test.java
+++ src/main/java/GitHub01Test.java
@@ -1,7 +1,8 @@
 public class GitHub01Test {
     public static void main(String[] args) {
         System.out.println("我正在测试上传我的第一个GitHub项目");
-
+        System.out.println("蔡徐坤到此一游");
+        
     }
 
 }
